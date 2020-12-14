9de4ab9dbbd66eedcc05562679166c4d6b204eb6
Y-wson <1002783067@qq.com>
2020-12-14 22:06:20 +0800

    fix:检验数据类型(main)

--- JavaScript的数据类型及其检测/检验数据类型.js
+++ JavaScript的数据类型及其检测/检验数据类型.js
@@ -6,5 +6,18 @@ typeof []; //object
 // 不能检测null和undefined
 console.log(1 instanceof Number); //false
 console.log(new Number(1) instanceof Number); //true
-
-// object.prototype.toString.call()最准确的常用方式
+console.log(null instanceof Null);
+// ReferenceError: Null is not defined
+// 对于特殊的数据类型null和undefined，他们的所属类是Null和Undefined，但是浏览器把这两个类保护起来了，不允许我们在外面访问使用。
+Object.prototype.toString.call(""); // [object String]
+Object.prototype.toString.call(1); // [object Number]
+Object.prototype.toString.call(true); // [object Boolean]
+Object.prototype.toString.call(undefined); // [object Undefined]
+Object.prototype.toString.call(null); // [object Null]
+Object.prototype.toString.call(new Function()); // [object Function]
+Object.prototype.toString.call(new Date()); // [object Date]
+Object.prototype.toString.call([]); // [object Array]
+Object.prototype.toString.call(new RegExp()); // [object RegExp]
+Object.prototype.toString.call(new Error()); // [object Error]
+Object.prototype.toString.call(document); // [object HTMLDocument]
+Object.prototype.toString.call(window); //[object global] window是全局对象global的引用
