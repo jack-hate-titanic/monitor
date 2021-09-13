8ea06f9073e02f17c2edaf8e7a96735ba98e453d
悦者生存 <1002783067@qq.com>
2021-09-13 23:33:06 +0800

    feat:ts下载

--- 正则表达式/正则表达式.js
+++ 正则表达式/正则表达式.js
@@ -1,14 +1,20 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-31 22:33:49
+ * @LastEditTime: 2021-09-06 22:08:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /frontDemo/正则表达式/正则表达式.js
+ */
 //[]
-const str = "there";
-const patter = /[a-h]/g;
-console.log(str.match(patter));
-
-console.log("---------------", "foo    bar".match(/(foo)\sbar/g));
+const str = "the.re";
+const patter = /\.re$/;
+console.log(patter.test(str));
 
 // 代码	说明
 // *	重复零次或更多次   如何记忆0*1 0
 // +	重复一次或更多次（至少有一次）0+1 1
-// ?	重复零次或一次（可有可无）返回true(1)或者false(0)
+// ?	重复零次或一次（可有可无）? 三目运算符返回0或者1
 // {n}	重复 n 次
 // {n,m}	至少 n 次最多 m 次
 // {n,}	至少 n 次
