ce05de0a13ab66954cce8d5759d9b95a5c4d6148
悦者生存 <1002783067@qq.com>
2021-10-02 22:32:19 +0800

    fix:数据精度

---
+++ 基础知识/symbol/symbol.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-01 10:42:23
+ * @LastEditTime: 2021-10-01 10:42:24
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /frontDemo/基础知识/symbol/symbol.js
+ */
+let s1 = Symbol("111");
+console.log(s1);
--- 基础知识/数据精度/toPrecision.js
+++ 基础知识/数据精度/toPrecision.js
@@ -1,5 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-13 17:57:14
+ * @LastEditTime: 2021-10-02 09:56:04
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /frontDemo/基础知识/数据精度/toPrecision.js
+ */
 function strip(num, precision = 12) {
   return parseFloat(num.toPrecision(precision));
 }
-console.log(strip(0.1 + 0.9999));
-console.log(0.1 + 1.8);
+console.log(strip(19.9 * 100));
+console.log(19.9 * 100);
