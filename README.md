b40a824a29d08124153964ba778a8e92fd5cc7de
悦者生存 <1002783067@qq.com>
2022-08-13 21:47:57 +0800

    fix: 添加测试代码

--- README.md
+++ README.md
@@ -2,15 +2,22 @@
  * @Author: 悦者生存 1002783067@qq.com
  * @Date: 2022-08-13 20:20:28
  * @LastEditors: 悦者生存 1002783067@qq.com
- * @LastEditTime: 2022-08-13 20:20:28
+ * @LastEditTime: 2022-08-13 21:22:33
  * @FilePath: /monitor/README.md
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 ## 前端采集脚本
 ## 监控错误
+### js错误
+### 资源加载错误
+### 异步错误
 ## 接口异常
 ## 白屏
 ## 加载时间
 ## 性能指标
 ## 卡顿
 ## pv
+
+## 后期计划
+- 1. 发布npm
+- 2. 服务器可配置化
---
+++ example/webpack.config.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: 悦者生存 1002783067@qq.com
+ * @Date: 2020-05-31 22:47:25
+ * @LastEditors: 悦者生存 1002783067@qq.com
+ * @LastEditTime: 2022-08-13 21:36:11
+ * @FilePath: /monitor/example/webpack.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+    entry: fs.readdirSync(__dirname).reduce((entries, dir) => {
+        const fullDir = path.join(__dirname, dir)
+        const entry = path.join(fullDir, 'app.js')
+        if (fs.statSync(fullDir).isDirectory() && fs.existsSync(entry)) {
+          entries[dir] =  [entry]
+        }
+        return entries
+      }, {}),
+    context: process.cwd(),//上下文目录 
+    mode: 'development',//开发模式
+    output: {
+        path: path.resolve(__dirname, 'dist'),//输出目录
+        filename: '[name].js',//文件名
+        publicPath: '/dist/'
+    },
+
+}
