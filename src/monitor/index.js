4139e22a8b6e94879b249a0abf3572e1378b3037
悦者生存 <1002783067@qq.com>
2022-08-14 21:44:19 +0800

    fix: 添加JsError测试用例

--- src/monitor/index.js
+++ src/monitor/index.js
@@ -2,7 +2,7 @@
  * @Author: 悦者生存 1002783067@qq.com
  * @Date: 2020-05-31 22:47:25
  * @LastEditors: 悦者生存 1002783067@qq.com
- * @LastEditTime: 2022-08-13 20:22:29
+ * @LastEditTime: 2022-08-14 21:36:09
  * @FilePath: /monitor/src/monitor/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,8 +10,5 @@ import { injectJsError } from './lib/jsError';
 import { injectXHR } from './lib/xhr';
 import { blankScreen } from './lib/blankScreen';
 import { timing } from './lib/timing';
-// 捕获js错误
-injectJsError();
-injectXHR();
-blankScreen();
-timing();
+
+export { injectJsError, injectXHR, blankScreen, timing };
--- src/monitor/utils/tracker.js
+++ src/monitor/utils/tracker.js
@@ -1,7 +1,12 @@
+/*
+ * @Author: 悦者生存 1002783067@qq.com
+ * @Date: 2020-05-31 22:47:25
+ * @LastEditors: 悦者生存 1002783067@qq.com
+ * @LastEditTime: 2022-08-14 21:43:51
+ * @FilePath: /monitor/src/monitor/utils/tracker.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 
-let host = 'cn-beijing.log.aliyuncs.com';
-let project = 'zhufengmonitor';
-let logStore = 'zhufengmonitor-store';
 let userAgent = require('user-agent');
 function getExtraData() {
     return {
@@ -15,7 +20,7 @@ function getExtraData() {
 //gif图片做上传 图片速度 快没有跨域 问题，
 class SendTracker {
     constructor() {
-        this.url = `http://${project}.${host}/logstores/${logStore}/track`;//上报的路径
+        this.url = "";//上报的路径
         this.xhr = new XMLHttpRequest;
     }
     send(data = {}) {
@@ -28,20 +33,15 @@ class SendTracker {
             }
         }
         console.log('log', log);
-        let body = JSON.stringify({
-            __logs__: [log]
-        });
-        this.xhr.open('POST', this.url, true);
-        this.xhr.setRequestHeader('Content-Type', 'application/json');//请求体类型
-        this.xhr.setRequestHeader('x-log-apiversion', '0.6.0');//版本号
-        this.xhr.setRequestHeader('x-log-bodyrawsize', body.length);//请求体的大小
-        this.xhr.onload = function () {
-            // console.log(this.xhr.response);
-        }
-        this.xhr.onerror = function (error) {
-            //console.log(error);
-        }
-        this.xhr.send(body);
+        // this.xhr.open('POST', this.url, true);
+        // this.xhr.setRequestHeader('Content-Type', 'application/json');//请求体类型
+        // this.xhr.onload = function () {
+        //     // console.log(this.xhr.response);
+        // }
+        // this.xhr.onerror = function (error) {
+        //     //console.log(error);
+        // }
+        // this.xhr.send(body);
     }
 }
 export default new SendTracker();
