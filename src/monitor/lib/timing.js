8b071970f0d269350d4ea9e74e57383d5c14c3ee
悦者生存 <1002783067@qq.com>
2022-08-16 08:44:21 +0800

    fix: 更新性能指标

--- src/monitor/lib/timing.js
+++ src/monitor/lib/timing.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 悦者生存 1002783067@qq.com
+ * @Date: 2020-05-31 22:47:25
+ * @LastEditors: 悦者生存 1002783067@qq.com
+ * @LastEditTime: 2022-08-16 08:33:28
+ * @FilePath: /monitor/src/monitor/lib/timing.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import tracker from '../utils/tracker';
 import onload from '../utils/onload';
 import getLastEvent from '../utils/getLastEvent';
